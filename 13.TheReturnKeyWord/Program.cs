9ca8ec7ee8055f4c79b77169e024ed5e08fc9d91
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-09 15:45:37 +0200

    Add project files.

---
+++ 13.TheReturnKeyWord/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.TheReturnKeyWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cube = 3;
+            
+            //calling a method with areturn type
+            Console.WriteLine(cube);
+            
+            //this wont give anything to console
+            
+
+            //freezing the console
+            Console.ReadLine();
+        }
+        //declare a method with a return type int
+
+        static int cubes(int number) 
+        { 
+            int results =number * number * number;
+            return results;
+            
+        }
+    }
+}
